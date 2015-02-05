44350ac224b978617e8410583337d0530a152079
micahhausler <micah.hausler@ambition.com>
2015-02-05 15:22:45 -0500

    Don't need to test for 3.2

--- setup.py
+++ setup.py
@@ -46,7 +46,6 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
